76e7d31d69fc78a36b354450b5a1317adf55efa0
yokkesan <lc.yteshima@gmail.com>
2026-03-02 08:25:31 +0900

    APIパス修正

--- api/src/main/java/com/jvspayout/api/HealthController.java
+++ api/src/main/java/com/jvspayout/api/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping("/api/health")
     public String health() {
         return "API OK";
     }
